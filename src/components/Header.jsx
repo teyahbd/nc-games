8b773675bc903c127e28250a283b8723f787ca88
teyahbd <teyahbrennendavies@gmail.com>
2022-10-31 14:55:29 +0000

    add basic header styling and functionality

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+import UserWidget from "./UserWidget";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar>
+        <Container>
+          <Navbar.Brand>NC Games</Navbar.Brand>
+          <Nav>
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <NavDropdown title="Categories">
+              <NavDropdown.Item>Category 1</NavDropdown.Item>
+            </NavDropdown>
+            <UserWidget />
+          </Nav>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/ReviewsView.jsx
@@ -0,0 +1,5 @@
+const ReviewsView = () => {
+  return <h2>Reviews view!</h2>;
+};
+
+export default ReviewsView;
