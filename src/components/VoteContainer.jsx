dc6f7f00cca104de9793800e5f2cd28ca4651560
teyahbd <teyahbrennendavies@gmail.com>
2022-11-04 12:16:47 +0000

    add more complex button functionality

--- src/components/VoteContainer.jsx
+++ src/components/VoteContainer.jsx
@@ -4,22 +4,48 @@ import * as api from "../api";
 const VoteContainer = ({ review, user, userVotesStr }) => {
   const [optimisticVoteInc, setOptimisticVoteInc] = useState(0);
   const [buttonStyling, setButtonStyling] = useState(["", ""]);
-  console.log(optimisticVoteInc);
-  console.log(userVotesStr, "updated");
 
   function handleClick(userVote) {
-    console.log("click", userVote);
-    setOptimisticVoteInc((currentVote) => currentVote + userVote);
+    if (buttonStyling[0] === "" && buttonStyling[1] === "") {
+      setOptimisticVoteInc((currentVote) => currentVote + userVote);
 
-    userVote === 1
-      ? setButtonStyling(["upvote", ""])
-      : setButtonStyling(["", "downvote"]);
-    api
-      .updateVotes(review.review_id, userVote)
-      .then(({ data }) => console.log(data));
+      userVote === 1
+        ? setButtonStyling(["upvote", ""])
+        : setButtonStyling(["", "downvote"]);
+      api
+        .updateVotes(review.review_id, userVote)
+        .then(({ data }) => console.log(data));
+    } else if (buttonStyling[0] === "upvote" && userVote === 1) {
+      setOptimisticVoteInc((currentVote) => currentVote - 1);
+
+      setButtonStyling(["", ""]);
+      api
+        .updateVotes(review.review_id, -1)
+        .then(({ data }) => console.log(data));
+    } else if (buttonStyling[1] === "downvote" && userVote === -1) {
+      setOptimisticVoteInc((currentVote) => currentVote + 1);
+
+      setButtonStyling(["", ""]);
+      api
+        .updateVotes(review.review_id, 1)
+        .then(({ data }) => console.log(data));
+    } else if (buttonStyling[0] === "upvote" && userVote === -1) {
+      setOptimisticVoteInc((currentVote) => currentVote - 2);
+
+      setButtonStyling(["", "downvote"]);
+      api
+        .updateVotes(review.review_id, -2)
+        .then(({ data }) => console.log(data));
+    } else if (buttonStyling[1] === "downvote" && userVote === 1) {
+      setOptimisticVoteInc((currentVote) => currentVote + 2);
+
+      setButtonStyling(["upvote", ""]);
+      api
+        .updateVotes(review.review_id, 2)
+        .then(({ data }) => console.log(data));
+    }
   }
 
-  console.log(buttonStyling);
   return (
     <div className="vote-container">
       <button
