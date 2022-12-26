0768ce22b9f479efbdee3a5b2c6c01ea398bd276
teyahbd <teyahbrennendavies@gmail.com>
2022-12-26 19:07:55 +0000

    add link back to user selection

--- src/components/header/UserWidget.jsx
+++ src/components/header/UserWidget.jsx
@@ -1,15 +1,19 @@
+import { Link } from "react-router-dom";
+
 const UserWidget = ({ user }) => {
   return (
-    <div id="user-widget">
-      <img src={`${user.avatar_url}`} alt="user avatar" id="user-avatar" />
-      <div id="user-widget-text" className="web-only">
-        <p className="web-only">{`${user.name}`}</p>
-        <p
-          id="user-widget-username"
-          className="web-only"
-        >{`${user.username}`}</p>
+    <Link to="/" id="user-link">
+      <div id="user-widget">
+        <img src={`${user.avatar_url}`} alt="user avatar" id="user-avatar" />
+        <div id="user-widget-text" className="web-only">
+          <p className="web-only">{`${user.name}`}</p>
+          <p
+            id="user-widget-username"
+            className="web-only"
+          >{`${user.username}`}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
